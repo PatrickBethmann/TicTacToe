824060173b9a756404ad314eb47746e3b0f72193
Patrick Bethmann <patrickbethmann@outlook.de>
2021-10-15 23:25:07 +0200

    Added many functions

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
 const gameboard = (() => {
-    const field = [[null, null, null], [null, null, null], [null, null,null]];
+    const divs = document.querySelectorAll(".field");
+
+    const field = [
+        [divs[0], divs[1], divs[2]],
+        [divs[3], divs[4], divs[5]],
+        [divs[6], divs[7], divs[8]],
+    ];
+
     const place = (sign, x, y) => {
-        field[x][y] = sign;
-    }
+        field[x][y].textContent = sign;
+    };
     const update = () => {
-        console.clear();
         console.log(field);
-    }
-    return {place, update};
+        return field;
+    };
+    const reset = () => {
+        divs.forEach((div) => {
+            div.textContent = "";
+            console.log("Field has been reset");
+        });
+    };
+    return { place, update, reset, field };
 })();
 
-const Player = (sign) => {
+const Player = () => {
+    Player.numberOfPlayers = Player.numberOfPlayers + 1 || 0;
+    console.log(Player.numberOfPlayers);
+    signs = ["X", "O", "H", "S"];
+    const sign = signs[Player.numberOfPlayers];
     const score = 0;
     function place(x, y) {
         gameboard.place(this.sign, x, y);
     }
-    return {score, sign, place}
-}
 
-const displayController = (() => {
-    
-})();
+    return { score, sign, place };
+};
+
+const displayController = (() => {})();
 
 const game = (() => {
-    let currentPlayer;
-    const player1 = Player("X");
-    const player2 = Player("O");
+    const player1 = Player();
+    const player2 = Player();
+    let currentPlayer = player1;
+    let round = 0;
 
-    player1.place(0,0);
-    player2.place(1,1)
+    const checkForWinner = () => {
+        let winner;
+    };
 
+    // Game Running
+    while (checkForWinner() === undefined) {
+        round++;
+        console.log("Current Round:", round);
+        if (round >= 10) {
+            break;
+        }
+    }
+
+    gameboard.field.forEach((row) => {
+        row.forEach((div) => {
+            div.addEventListener("click", () => {
+                console.log("working");
+                let x = gameboard.field.indexOf(row);
+                let y = row.indexOf(div);
+
+                console.log([x, y]);
+                currentPlayer.place(x, y);
+                currentPlayer === player1
+                    ? (currentPlayer = player2)
+                    : (currentPlayer = player1);
+            });
+        });
+    });
+
+    console.log("Game end");
     gameboard.update();
+    console.log(gameboard);
 })();
-
-
-
